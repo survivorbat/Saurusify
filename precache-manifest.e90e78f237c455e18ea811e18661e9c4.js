c55f6eb753726172a81f935baffe5fd3bbc660c0
survivorbat <djbatcat@gmail.com>
2022-05-06 09:47:05 +0200

    Updates

---
+++ precache-manifest.e90e78f237c455e18ea811e18661e9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1881cb142bd2a9ffafe",
+    "url": "/static/css/main.f8631c29.chunk.css"
+  },
+  {
+    "revision": "f1881cb142bd2a9ffafe",
+    "url": "/static/js/main.43e7c1ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "31f782eac61069b01597",
+    "url": "/static/css/2.28ac61b5.chunk.css"
+  },
+  {
+    "revision": "31f782eac61069b01597",
+    "url": "/static/js/2.6dcf8cf4.chunk.js"
+  },
+  {
+    "revision": "f3ec491e6e0c431011ae40e89523d445",
+    "url": "/index.html"
+  }
+];
